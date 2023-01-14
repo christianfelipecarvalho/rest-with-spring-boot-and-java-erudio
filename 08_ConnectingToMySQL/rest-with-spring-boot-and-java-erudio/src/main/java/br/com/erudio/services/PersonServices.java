216f2eeb31d8b55b67b22030054eacee5df1cad6
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2023-01-14 15:54:56 -0300

    Criando banco de dados

---
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,88 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public List<Person> findAll() {
+		
+		logger.info("Finding all people!");
+		List<Person> persons = new ArrayList<>();
+		for(int i = 0; i < 10; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+		
+	}
+	
+	public Person findById(String id) {
+	
+		
+		logger.info("Finding one person!");
+		
+		Person person = new Person();
+		
+		person.setId(counter.incrementAndGet());
+		person.setFirtName("Christian");
+		person.setLastName("Carvalho");
+		person.setAddress("Criciúma - Santa Catarina - Brasil");
+		person.setGender("male");
+		return person;
+	}
+	
+	public Person create(Person person) {
+
+		logger.info("create one person!");
+		return person;
+	}
+	
+	public Person Update(Person person) {
+
+		logger.info("update one person!");
+		
+		return person;
+	}
+	
+//	public void delete(String id) {
+//
+//		logger.info("Deleting one person!");
+//		
+//	}
+	
+	
+	private Person mockPerson(int i) {
+		
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirtName("Person name " + i);
+		person.setLastName("Last name "+ i);
+		person.setAddress("Some address in BRasil" + i);
+		person.setGender("male");
+		return person;
+		
+	}
+
+	public void delete(String id) {
+		logger.info("Deleting one person!");
+		
+	}
+
+	
+
+
+}
