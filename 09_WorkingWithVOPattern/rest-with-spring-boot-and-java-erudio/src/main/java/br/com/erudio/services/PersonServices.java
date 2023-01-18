6739804f02c7c3b7d295e50d9253c3ffbd8b5a4c
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2023-01-17 21:31:23 -0300

    Criando nova pasta para edição de VO (value object) pattern

---
+++ 09_WorkingWithVOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,71 @@
+package br.com.erudio.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+
+
+@Service
+public class PersonServices {
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+	public List<Person> findAll() {
+		
+		logger.info("Finding all people!");
+
+		return repository.findAll();
+		
+	}
+	
+	public Person findById(Long id) {
+	
+		
+		logger.info("Finding one person!");
+		
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	}
+	
+	public Person create(Person person) {
+
+		logger.info("create one person!");
+		return repository.save(person);
+	}
+	
+	public Person Update(Person person) {
+
+		logger.info("update one person!");
+		
+		var entity = repository.findById(person.getId())
+		.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		entity.setFirtName(person.getFirtName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person);
+	}
+
+
+	public void delete(Long id) {
+		logger.info("Deleting one person!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+
+	
+
+
+}
