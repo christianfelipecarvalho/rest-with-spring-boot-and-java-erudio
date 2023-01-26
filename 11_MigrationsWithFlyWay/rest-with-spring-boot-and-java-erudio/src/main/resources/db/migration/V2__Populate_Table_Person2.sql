ff4645997728f96831d2c53e7e05412bdb4d6758
christianfelipecarvalho <christianfc001@gmail.com>
2023-01-25 22:40:57 -0300

    configurando migrations

---
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`, `last_name`)VALUES
+ (1,'Criciuma','Christian','Male','Carvalho'),
+ (2,'içara','Joao','Male','Carvalho'),
+ (3,'Jirandina - california','Nikilas','Male','tesla'),
+ (4,'Criciuma - Santa catarina','Paula Mendonça','Female','Carvalho');
+
+
