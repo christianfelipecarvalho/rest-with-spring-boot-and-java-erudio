2b36144007c95c617760fc5013a730979b158487
christianfelipecarvalho <christianfc001@gmail.com>
2023-01-27 20:07:07 -0300

    Adicionando migrations e flyway

--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -2,25 +2,25 @@ package br.com.erudio.data.vo.v1;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 
-@JsonPropertyOrder({"id","first_name", "last_name", "address", "gender"})
+//@JsonPropertyOrder({"id","first_name", "last_name", "address", "gender"})
 public class PersonVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	
-	@JsonProperty("first_name")
+	//@JsonProperty("first_name")
 	private String firstName;
 	
-	@JsonProperty("last_name")
+	//@JsonProperty("last_name")
 	private String lastName;
 	
-	@JsonIgnore
+	//@JsonIgnore
 	private String address;
 	
 	private String gender;
